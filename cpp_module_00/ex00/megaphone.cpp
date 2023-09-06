27e4110661d3fc89b404fa2679669df62be01458
Dean Ruina <druina@c2r5p11.hive.fi>
2023-09-06 09:18:45 +0300

    ex00 done starting ex01

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -3,16 +3,13 @@
 int main (int argc, char **argv)
 {
 	if (argc == 1)
-		std::cout << " * LOUD AND UNBEARABLE FEEDBACK NOISE * " << std::endl;
-	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	for (int i = 1; argv[i]; i++)
 	{
-		for (int i = 1; argv[i]; i++)
-		{
-			for (int j = 0; argv[i][j]; j++)
-				argv[i][j] = toupper(argv[i][j]);
-			std::cout << argv[i] << " ";
-		}
-			std::cout << std::endl;
+		for (int j = 0; argv[i][j]; j++)
+			argv[i][j] = toupper(argv[i][j]);
+		std::cout << argv[i] << ' ';
 	}
+	std::cout << std::endl;
 	return (0);
 }
