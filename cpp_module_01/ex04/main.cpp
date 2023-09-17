9c283c58b5d04690d03b2f22bb149a3a0653fa5e
Dean Ruina <ruinadd@gmail.com>
2023-09-17 20:27:12 +0300

    ex04 done

---
+++ cpp_module_01/ex04/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+  if (argc != 4)
+    return(std::cout << "3 argument's needed" << std::endl, 1);
+
+  std::string fileName = argv[1], find = argv[2], replace = argv[3];
+  std::ifstream inFile(fileName.c_str());
+  if (inFile.fail())
+    return(std::cout << "Openning input file failed" << std::endl, 1);
+  std::ofstream outFile((fileName + ".replace").c_str());
+  if (inFile.fail())
+    return(std::cout << "Openning output file failed" << std::endl, 1);
+
+  size_t index;
+  for (std::string line; std::getline(inFile, line);)
+  {
+    while ((index = line.find(find)) != std::string::npos && find.compare(""))
+    {
+      line.erase(index, find.length());
+      line.insert(index, replace);
+    }
+    outFile << line << std::endl;
+  }
+  inFile.close();
+  outFile.close();
+  return (0);
+
+}
