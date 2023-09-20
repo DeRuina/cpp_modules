24f1aaacea9b63c8626d9af62c7524ca805720ed
Dean Ruina <druina@c2r5p11.hive.fi>
2023-09-20 14:25:32 +0300

    ex02 done

--- cpp_module_02/ex02/Fixed.cpp
+++ cpp_module_02/ex02/Fixed.cpp
@@ -1,5 +1,7 @@
 #include "Fixed.hpp"
 
+// Constructors / Destructors
+
 Fixed::Fixed() : x(0)
 {
 	std::cout << "Default constructor called" << std::endl;
@@ -31,6 +33,8 @@ Fixed::~Fixed()
 	std::cout << "Destructor called" << std::endl;
 }
 
+//Getter / Setter
+
 int Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
@@ -42,6 +46,9 @@ void Fixed::setRawBits(int const raw)
 	std::cout << "setRawBits member function called" << std::endl;
 	this->x = raw;
 }
+
+// toFloat / toInt
+
 float Fixed::toFloat(void) const
 {
 	return (((float)x / (float)(1 << this->y)));
@@ -51,6 +58,128 @@ int Fixed::toInt(void) const
 {
 	return (x >> this->y);
 }
+
+//Comparison Operators Overload
+
+bool Fixed::operator==(const Fixed &equal)
+{
+	return (this->x == equal.getRawBits());
+}
+
+bool Fixed::operator!=(const Fixed &not_equal)
+{
+	return (this->x == not_equal.getRawBits());
+}
+
+bool Fixed::operator<=(const Fixed &bigger_equal)
+{
+	return (this->x <= bigger_equal.getRawBits());
+}
+
+bool Fixed::operator>=(const Fixed &smaller_equal)
+{
+	return (this->x >= smaller_equal.getRawBits());
+}
+
+bool Fixed::operator>(const Fixed &smaller)
+{
+	return (this->x > smaller.getRawBits());
+}
+
+bool Fixed::operator<(const Fixed &bigger)
+{
+	return (this->x < bigger.getRawBits());
+}
+
+//Arithmetic Operators Overload
+
+Fixed Fixed::operator+(const Fixed &add)
+{
+	return(Fixed(this->toFloat() + add.toFloat()));
+}
+
+Fixed Fixed::operator-(const Fixed &subtract)
+{
+	return(Fixed(this->toFloat() - subtract.toFloat()));
+}
+
+Fixed Fixed::operator*(const Fixed &multiply)
+{
+	return(Fixed(this->toFloat() * multiply.toFloat()));
+}
+
+Fixed Fixed::operator/(const Fixed &divide)
+{
+	if (divide.getRawBits() != 0)
+		return(Fixed(this->toFloat() / divide.toFloat()));
+	else
+		return (std::cout << "can't divide by zero" << std::endl, Fixed());
+}
+
+//Prefix
+
+Fixed &Fixed::operator++()
+{
+	this->x++;
+	return (*this);
+}
+
+Fixed &Fixed::operator--()
+{
+	this->x--;
+	return (*this);
+}
+
+//Postfix
+
+Fixed Fixed::operator++(int)
+{
+	Fixed postfix(*this);
+	this->x++;
+	return(postfix);
+}
+
+Fixed Fixed::operator--(int)
+{
+	Fixed postfix(*this);
+	this->x--;
+	return(postfix);
+}
+
+//Min / Max
+
+Fixed &Fixed::min(Fixed &x, Fixed &y)
+{
+	if (x.getRawBits() < y.getRawBits())
+		return (x);
+	else
+		return (y);
+}
+
+const Fixed &Fixed::min(const Fixed &x, const Fixed &y)
+{
+	if (x.getRawBits() < y.getRawBits())
+		return (x);
+	else
+		return (y);
+}
+Fixed &Fixed::max(Fixed &x, Fixed &y)
+{
+	if (x.getRawBits() > y.getRawBits())
+		return (x);
+	else
+		return (y);
+}
+
+const Fixed &Fixed::max(const Fixed &x, const Fixed &y)
+{
+	if (x.getRawBits() > y.getRawBits())
+		return (x);
+	else
+		return (y);
+}
+
+//Insertion Overload
 
 std::ostream &operator<<(std::ostream &OUT,const Fixed &fixed)
 {
--- cpp_module_02/ex02/Fixed.hpp
+++ cpp_module_02/ex02/Fixed.hpp
@@ -18,10 +18,35 @@ class Fixed
 		~Fixed();
 		Fixed(const Fixed &copy);
 		Fixed &operator=(const Fixed &src);
+
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
 		float toFloat(void) const;
 		int toInt(void) const;
+
+		bool operator==(const Fixed &equal);
+		bool operator!=(const Fixed &not_equal);
+		bool operator<=(const Fixed &bigger_equal);
+		bool operator>=(const Fixed &smaller_equal);
+		bool operator>(const Fixed &smaller);
+		bool operator<(const Fixed &bigger);
+
+		Fixed operator+(const Fixed &add);
+		Fixed operator-(const Fixed &subtract);
+		Fixed operator*(const Fixed &multiply);
+		Fixed operator/(const Fixed &divide);
+
+		Fixed &operator++();
+		Fixed &operator--();
+		Fixed operator++(int);
+		Fixed operator--(int);
+
+		static Fixed &max(Fixed &x, Fixed &y);
+		static const Fixed &max(const Fixed &x, const Fixed &y);
+		static Fixed &min(Fixed &x, Fixed &y);
+		static const Fixed &min(const Fixed &x, const Fixed &y);
+
+
 };
 
 std::ostream &operator<<(std::ostream &OUT,const Fixed &fixed);
