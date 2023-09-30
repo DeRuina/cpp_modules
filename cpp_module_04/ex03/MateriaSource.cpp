cc4b1cf5a4aee6756289b380749fdbb4797abe21
Dean Ruina <ruinadd@gmail.com>
2023-10-01 00:12:47 +0300

    added IMateriaSource and MateriaSource

---
+++ cpp_module_04/ex03/MateriaSource.cpp
@@ -0,0 +1,64 @@
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource()
+{
+	for (int i = 0; i < 4; i++)
+		materias[i] = NULL;
+}
+MateriaSource::MateriaSource(const MateriaSource &copy)
+{
+	for (int i = 0; i < 4; i++)
+		materias[i] = copy.getMateria(i)->clone();
+}
+
+MateriaSource &MateriaSource::operator=(const MateriaSource &src)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		delete materias[i];
+		materias[i] = src.getMateria(i)->clone();
+	}
+	return (*this);
+}
+
+MateriaSource::~MateriaSource()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		delete materias[i];
+		materias[i] = NULL;
+	}
+}
+void	MateriaSource::learnMateria(AMateria *materia)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (materias[i] == NULL && materia)
+		{
+			materias[i] = materia;
+			return;
+		}
+	}
+	  std::cout << "Didn't learn" << std::endl;
+}
+
+AMateria	*MateriaSource::createMateria(std::string const &type)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (materias[i] != NULL && materias[i]->getType() == type)
+		{
+			std::cout << "Materia "<< type << " created."<< std::endl;
+			return (materias[i]->clone());
+		}
+	}
+	std::cout << "Didn't create" << std::endl;
+	return (NULL);
+}
+
+AMateria *MateriaSource::getMateria(int i) const
+{
+  if (i < 0 || i > 3) 
+		return (NULL);
+	return (materias[i]);
+}
---
+++ cpp_module_04/ex03/MateriaSource.hpp
@@ -0,0 +1,24 @@
+#ifndef MATERIASOURCE_H
+#define MATERIASOURCE_H
+
+#include "IMateriaSource.hpp"
+
+class MateriaSource : public IMateriaSource
+{
+  private:
+	    AMateria *materias[4];
+
+  public:
+	    MateriaSource();
+	    MateriaSource(const MateriaSource &copy);
+      MateriaSource &operator=(const MateriaSource &src);
+
+	    virtual void learnMateria(AMateria *materia);
+	    virtual AMateria *createMateria(std::string const &type);
+
+	    AMateria *getMateria(int i) const;
+
+
+};
+
+#endif
