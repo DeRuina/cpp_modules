a0ed5c8beb64ce58319cc59947e3cf2c62eac06f
Dean Ruina <ruinadd@gmail.com>
2023-10-06 15:56:04 +0300

    classes build, still working on the functions

--- cpp_module_05/ex00/Bureaucrat.cpp
+++ cpp_module_05/ex00/Bureaucrat.cpp
@@ -1,1 +1,52 @@
 #include "Bureaucrat.hpp"
+
+virtual const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+  return ("Grade too high");
+}
+
+virtual const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+  return ("Grade too low");
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) 
+{
+  this->name = name;
+  if (grade  < 1)
+    throw GradeTooHighException();
+  if (grade > 150)
+    throw GradeTooLowException();
+  this->grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy)
+{
+  *this = copy;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &src)
+{
+  this->name = src.name;
+  if (src.grade < 1)
+    throw GradeTooHighException();
+  if (src.grade > 150)
+    throw GradeTooLowException();
+  this->grade = src.grade;
+  return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{ 
+}
+
+const &std::string Bureaucrat::getName() const
+{
+  return (this->name);
+} 
+
+int Bureaucrat::getGrade() const
+{
+  return (this->grade);
+}
+
--- cpp_module_05/ex00/Bureaucrat.hpp
+++ cpp_module_05/ex00/Bureaucrat.hpp
@@ -3,6 +3,37 @@
 
 #include <iostream>
 #include <string>
+#include <stdexcept>
+
+class Bureaucrat
+{
+  private:
+      const std::string name;
+      int grade;
+  
+  public:
+      class GradeTooHighException : public std::excpetion
+      {
+        virtual const char* what() const throw();
+      };
+      class GradeTooLowException : public std::exception 
+      {
+        virtual const char* what() const throw();
+      };
+
+      Bureaucrat(std::string name, int grade);
+      Bureaucrat(const Bureaucrat &copy);
+      Bureaucrat &operator=(const Bureaucrat &src);
+      ~Bureaucrat();
+
+      const       std::string &getName() const;
+      int         getGrade() const;
+      void        incrementGrade();
+      void        decrementGrade();
+
+      std::ostream &operator<<(std::ostream &OUT, const Bureaucrat &bureaucrat);
+
+};
 
 
 #endif
