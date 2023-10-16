cb79baeb7b8148f6dfa43ecae680310cbe6d8b7e
Dean Ruina <ruinadd@gmail.com>
2023-10-16 23:13:07 +0300

    AForm abstarct class ready & added execute function to Bureaucrat

---
+++ cpp_module_05/ex02/AForm.cpp
@@ -0,0 +1,78 @@
+#include "Form.hpp"
+
+AForm::AForm(std::string name, int signGrade, int executeGrade) : name(name), isSigned(false), signGrade(signGrade), executeGrade(executeGrade)
+{
+	if ( signGrade < 1 )
+		throw AForm::GradeTooHighException();
+    if ( signGrade > 150 )
+        throw AForm::GradeTooLowException();
+}
+AForm::AForm(const AForm &copy) : name(copy.name), signGrade(copy.signGrade), executeGrade(copy.executeGrade)
+{
+}
+
+AForm &AForm::operator=(const AForm &src)
+{
+	if ( signGrade < 1 )
+		throw AForm::GradeTooHighException();
+    if ( signGrade > 150 )
+        throw AForm::GradeTooLowException();
+	this->isSigned = src.isSigned;
+	return (*this);
+}
+
+AForm::~AForm()
+{
+}
+std::string	AForm::getName() const
+{
+	return (this->name);
+}
+
+bool	AForm::getSigned() const
+{
+	return (this->isSigned);
+}
+
+int	AForm::getGradeToSign() const
+{
+	return (this->signGrade);
+}
+
+int	AForm::getGradeToExecute() const
+{
+	return (this->executeGrade);
+}
+
+void	AForm::beSigned(const Bureaucrat& bureaucrat)
+{
+	if (bureaucrat.getGrade() > this->signGrade)
+		throw AForm::GradeTooLowException();
+	isSigned = true;
+}
+
+const char* AForm::GradeTooHighException::what() const throw()
+{
+  return ("Grade too high");
+}
+
+const char* AForm::GradeTooLowException::what() const throw()
+{
+  return ("Grade too low");
+}
+
+const char* AForm::FormNotSignedException::what() const throw()
+{
+  return ("Form not signed");
+}
+
+std::ostream &operator<<(std::ostream &OUT, const AForm &form)
+{
+	OUT << "Form name : " << form.getName() << std::endl <<
+	"Form is signed : " << form.getSigned() << std::endl <<
+	"Grade to sign : " << form.getGradeToSign() << std::endl <<
+	"Grade to execute : " << form.getGradeToExecute() << std::endl;
+	return (OUT);
+}
+
+
---
+++ cpp_module_05/ex02/AForm.hpp
@@ -0,0 +1,52 @@
+#ifndef AFORM_H
+#define AFORM_H
+
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class AForm
+{
+	private:
+		const					std::string	name;
+		bool					isSigned;
+		const int			signGrade;
+		const int			executeGrade;
+
+  public:
+			AForm(std::string name, int signGrade, int executeGrade);
+			AForm(const AForm &copy);
+			AForm &operator=(const AForm &src);
+			virtual ~AForm();
+
+			std::string	getName() const;
+			bool				getSigned() const;
+			int					getGradeToSign() const;
+			int					getGradeToExecute() const;
+			void				beSigned(const Bureaucrat &bureaucrat);
+
+			virtual void        execute( const Bureaucrat& executor ) const = 0;
+
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+	class FormNotSignedException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+
+
+};
+
+std::ostream &operator<<(std::ostream &OUT, const AForm &form);
+
+#endif
