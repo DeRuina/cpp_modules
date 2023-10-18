5bf212b5486ffb2e1147d1ad2211190b36993679
Dean Ruina <druina@c1r5p21.hive.fi>
2023-10-18 10:14:43 +0300

    Form changed to AForm in hpp file

--- cpp_module_05/ex02/Bureaucrat.hpp
+++ cpp_module_05/ex02/Bureaucrat.hpp
@@ -24,8 +24,8 @@ class Bureaucrat
 			int					getGrade() const;
 			void				incrementGrade();
 			void				decrementGrade();
-			void				signForm(Form &form) const;
-			void				executeForm( const Form &form ) const;
+			void				signForm(AForm &form) const;
+			void				executeForm( const AForm &form ) const;
 
 		class GradeTooHighException : public std::exception
 		{
