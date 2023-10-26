ef0cc762ef46a84f867c2e7849022b12fa3b5889
Dean Ruina <ruinadd@gmail.com>
2023-10-26 14:34:42 +0300

    ex00 done

--- cpp_module_07/ex00/whatever.hpp
+++ cpp_module_07/ex00/whatever.hpp
@@ -1,5 +1,32 @@
 #ifndef WHATEVER_H
 #define WHATEVER_H
 
+#include <iostream>
+
+template <typename T>
+void swap(T &arg1, T &arg2)
+{
+  T temp;
+  temp = arg1;
+  arg1 = arg2;
+  arg2 = temp;
+}
+
+template <typename T>
+T min(T &arg1, T &arg2)
+{
+  if (arg2 <= arg1)
+    return (arg2);
+  return (arg1);
+}
+
+template <typename T>
+T max(T &arg1, T &arg2)
+{
+  if (arg2 >= arg1)
+    return (arg2);
+  return (arg1);
+}
+
 
 #endif
