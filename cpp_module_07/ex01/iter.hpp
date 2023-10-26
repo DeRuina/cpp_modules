7910cd6a30d80a0c5fc749c1b5464f721d26fc3c
Dean Ruina <ruinadd@gmail.com>
2023-10-26 14:56:07 +0300

    ex01 done

--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -1,6 +1,19 @@
 #ifndef ITER_H
 #define ITER_H
 
+#include <iostream>
 
+template <typename T>
+void printValue(T & val)
+{
+  std::cout << val << std::endl;
+}
+
+template <typename T>
+void iter(T arr[], int size, void (*func)(T&))
+{
+  for (int i = 0; i < size; i++)
+    func(arr[i]);
+}
 
 #endif
--- cpp_module_07/ex01/main.cpp
+++ cpp_module_07/ex01/main.cpp
@@ -2,5 +2,14 @@
 
 int main(void)
 {
+
+  int arr[] = {1, 4, 7, 12312};
+  char str[] = "EL EL ISRAEL";
+  bool boolArr[] = {true, true, false};
+
+  iter(arr, 4, printValue);
+  iter(str, 12, printValue);
+  iter(boolArr, 3, printValue);
+
   return (0);
 }
