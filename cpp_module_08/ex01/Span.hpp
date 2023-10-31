69ed7ee065ff040f8d97de64d6770491c7203ad0
Dean Ruina HIVE computer <ruinadd@gmail.com>
2023-10-31 15:21:34 +0200

    function declaration finished

--- cpp_module_08/ex01/Span.hpp
+++ cpp_module_08/ex01/Span.hpp
@@ -1,5 +1,31 @@
 #ifndef SPAN_H
 #define SPAN_H
 
+#include <iostream>
+#include <vector>
+#include <exception>
+#include <algorithm>
+
+class Span
+{
+	private:
+		unsigned int N;
+		std::vector<int> vec;
+
+	public:
+		Span(unsigned int N);
+		Span(const Span &copy);
+		Span &operator=(const Span &src);
+		~Span();
+
+		void addNumber(int num);
+		void addNumber(std::vector<int>::iterator it1, std::vector<int>::iterator it2);
+		unsigned int shortestSpan();
+		unsigned int longestSpan();
+
+
+
+};
+
 
 #endif
