89f7028c8d97157cad31f8f0a89ddf718f1f4cdc
Dean Ruina HIVE computer <ruinadd@gmail.com>
2023-10-31 11:44:17 +0200

    ex00 done

--- cpp_module_08/ex00/easyfind.hpp
+++ cpp_module_08/ex00/easyfind.hpp
@@ -1,5 +1,19 @@
 #ifndef EASYFIND_H
 #define EASYFIND_H
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <deque>
+
+template<typename T>
+int &easyfind(T container, int num)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), num);
+	if (it != container.end())
+		return (*it);
+	else
+		throw std::exception();
+}
 
 #endif
--- cpp_module_08/ex00/main.cpp
+++ cpp_module_08/ex00/main.cpp
@@ -2,5 +2,45 @@
 
 int main()
 {
+	std::vector<int> vec;
+	vec.push_back(40);
+	vec.push_back(41);
+	vec.push_back(42);
+	vec.push_back(43);
+	vec.push_back(44);
+	std::cout << easyfind(vec, 40) << std::endl;
+	std::cout << easyfind(vec, 42) << std::endl;
+	std::cout << easyfind(vec, 44) << std::endl;
+
+
+	std::deque<int> deq;
+	deq.push_front(0);
+	deq.push_front(1);
+	deq.push_front(2);
+	deq.push_front(3);
+	deq.push_front(4);
+
+	std::cout << easyfind(deq, 0) << std::endl;
+	std::cout << easyfind(deq, 3) << std::endl;
+	std::cout << easyfind(deq, 4) << std::endl;
+
+	try
+	{
+	std::cout << easyfind(vec, 50) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "not found" << std::endl;
+	}
+
+	try
+	{
+		std::cout << easyfind(deq, 42) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "not found" << std::endl;
+	}
+
 	return (0);
 }
