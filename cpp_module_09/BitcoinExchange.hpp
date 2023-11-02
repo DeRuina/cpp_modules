154f59e1ef71d06f4e4c0260b0567aaff7811610
Dean Ruina HIVE computer <ruinadd@gmail.com>
2023-11-02 12:05:59 +0200

    class created

--- cpp_module_09/BitcoinExchange.hpp
+++ cpp_module_09/BitcoinExchange.hpp
@@ -4,6 +4,20 @@
 #include <map>
 #include <string>
 
+class BitcoinExchange
+{
+	private:
+		std::map<std::string, double> data;
 
+		std::map<std::string, double> parce_exchange(std::string fileName);
+
+	public:
+		BitcoinExchange(std::string fileName);
+		BitcoinExchange(const BitcoinExchange &copy);
+		BitcoinExchange &operator=(const BitcoinExchange &src);
+		~BitcoinExchange();
+
+
+};
 
 #endif
