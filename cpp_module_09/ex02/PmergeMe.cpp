8690e7b879cf279edbde7b4025f761c21b31d37e
Dean Ruina HIVE computer <ruinadd@gmail.com>
2023-11-09 11:01:30 +0200

    starting to build the class

--- cpp_module_09/ex02/PmergeMe.cpp
+++ cpp_module_09/ex02/PmergeMe.cpp
@@ -1,1 +1,11 @@
 #include "PmergeMe.hpp"
+
+void PmergeMe::sort_sequence(std::string sequence)
+{
+	for (int i = 0; i < sequence.length(); i++)
+		if (std::isalpha(sequence[i]) || sequence[i] < 0)
+			std::cout << "Error: Invalid arguments" << std::endl;
+	std::vector<unsigned int> vec;
+	std::deque<unsigned int> deq;
+
+}
--- cpp_module_09/ex02/PmergeMe.hpp
+++ cpp_module_09/ex02/PmergeMe.hpp
@@ -3,6 +3,21 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <deque>
+
+class PmergeMe
+{
+	private:
+		PmergeMe();
+		PmergeMe(const PmergeMe &copy);
+		PmergeMe &operator=(const PmergeMe &src);
+		~PmergeMe();
+
+	public:
+		static void sort_sequence(std::string sequence);
+
+};
 
 
 
