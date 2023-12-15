454753170dd5db26684effabaadec8f67b2f15ab
Dean Ruina HIVE computer <ruinadd@gmail.com>
2023-12-15 08:39:06 +0200

    minor chnages for estatics

--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -6,14 +6,14 @@
 template <typename T>
 void printValue(T & val)
 {
-  std::cout << val << std::endl;
+	std::cout << val << std::endl;
 }
 
 template <typename T>
 void iter(T arr[], int size, void (*func)(T&))
 {
-  for (int i = 0; i < size; i++)
-    func(arr[i]);
+	for (int i = 0; i < size; i++)
+		func(arr[i]);
 }
 
 #endif
--- cpp_module_07/ex02/Array.hpp
+++ cpp_module_07/ex02/Array.hpp
@@ -5,19 +5,19 @@
 template <typename T>
 class Array
 {
-  private:
-      int arrSize;
-      T *arr;
+	private:
+		int arrSize;
+		T *arr;
 
-  public:
-      Array();
-      Array(unsigned int n);
-      Array(const Array &copy);
-      Array &operator=(const Array &src);
-      ~Array();
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array &copy);
+		Array &operator=(const Array &src);
+		~Array();
 
-      T &operator[](int index);
-      int size();
+		T &operator[](int index);
+		int size();
 };
 
 #include "Array.tpp"
