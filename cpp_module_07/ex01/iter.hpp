20d2a905080c1362271dfb38a82fc2a36e22aed1
Dean Ruina HIVE computer <ruinadd@gmail.com>
2023-12-15 14:42:23 +0200

    const overload fixed

--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -4,9 +4,16 @@
 #include <iostream>
 
 template <typename T>
-void printValue(T & val)
+void printValue(T const & val)
 {
 	std::cout << val << std::endl;
+}
+
+ template <typename T>
+void iter(T arr[], int size, void (*func)(T const &))
+{
+	for (int i = 0; i < size; i++)
+		func(arr[i]);
 }
 
 template <typename T>
