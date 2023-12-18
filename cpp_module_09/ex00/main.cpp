c0c1ca26c835073073fd83f6a4a24e22bff6f8dc
Dean Ruina HIVE computer <ruinadd@gmail.com>
2023-12-18 09:59:29 +0200

    updated error message

--- cpp_module_09/ex00/main.cpp
+++ cpp_module_09/ex00/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
 	if (argc != 2)
-		return (std::cout << "Error: could not open file." << std::endl, 1);
+		return (std::cout << "Error: Not enough arguments" << std::endl, 1);
 	BitcoinExchange ex;
 	ex.your_portfolio_values(argv[1]);
 	return (0);
